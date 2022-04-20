d89dc7f6454a0c105177c4daa78115f5003516bb
Felipe Pereira <felipe.pereira@tiendeo.com>
2022-04-20 17:32:44 +0200

    add retrofit and hilt dependency

--- app/src/main/java/com/felipe/popularmovies/mobile/mainView/MainActivity.kt
+++ app/src/main/java/com/felipe/popularmovies/mobile/mainView/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.felipe.popularmovies
+package com.felipe.popularmovies.mobile.mainView
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.felipe.popularmovies.R
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
