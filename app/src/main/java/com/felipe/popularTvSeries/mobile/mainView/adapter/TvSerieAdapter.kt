b1c015599048cb7317131633331b46998e5d8dfa
Felipe Pereira <felipe.pereira@tiendeo.com>
2022-04-21 19:01:35 +0200

    add glide

--- app/src/main/java/com/felipe/popularTvSeries/mobile/mainView/adapter/TvSerieAdapter.kt
+++ app/src/main/java/com/felipe/popularTvSeries/mobile/mainView/adapter/TvSerieAdapter.kt
@@ -3,7 +3,9 @@ package com.felipe.popularTvSeries.mobile.mainView.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.felipe.popularTvSeries.mobile.mainView.model.TvSerieViewEntity
 import com.felipe.populartvseries.R
 import com.felipe.populartvseries.databinding.AdapterTvSerieBinding
@@ -31,7 +33,23 @@ class TvSerieAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     fun bind(item: TvSerieViewEntity) {
       with(AdapterTvSerieBinding.bind(itemView)) {
         title.text = item.originalName
+        score.text = item.voteAverage
+        poster.load("$BASE_URL${item.posterImage}")
       }
     }
   }
+
+  fun ImageView.load(url: String?) {
+    Glide.with(this).load(url).into(this)
+//    val glideUrl = GlideUrl(url, headers)
+//    val requestBuilder = Glide.with(context)
+//      .load(glideUrl)
+//      .signature(GlideIntegerVersionSignature(context))
+//      .apply(RequestOptions().downsample(DownsampleStrategy.AT_LEAST))
+//    placeholderId?.let { requestBuilder.placeholder(placeholderId) }
+//    requestBuilder.into(this)
+  }
+
 }
+
+private const val BASE_URL = "https://image.tmdb.org/t/p/w500"
