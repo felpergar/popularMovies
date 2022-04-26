3d55f3f7f71c1bd20854456546cde5c3f61ea0f0
Felipe Pereira <felipe.pereira@tiendeo.com>
2022-04-26 08:25:29 +0200

    move file

--- app/src/main/java/com/felipe/popularTvSeries/mobile/di/ActivityProvider.kt
+++ app/src/main/java/com/felipe/popularTvSeries/mobile/di/ActivityProvider.kt
@@ -1,6 +1,7 @@
-package com.felipe.popularTvSeries.mobile.detail
+package com.felipe.popularTvSeries.mobile.di
 
 import android.app.Activity
+import com.felipe.popularTvSeries.mobile.detail.DetailActivity
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
